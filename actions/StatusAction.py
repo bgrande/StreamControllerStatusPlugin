66af8cbc78578fc35048f4f0255d004b5ad615ed
Benedikt Grande <dev@bgrande.de>
2026-01-02 16:04:45 +0100

    fix(settings): fix settings

--- actions/StatusAction.py
+++ actions/StatusAction.py
@@ -58,7 +58,7 @@ class StatusAction(ActionBase):
         #self.perform_check_async()
 
     def on_tick(self):
-        interval = self.settings["interval"]
+        interval = self.settings.get("interval")
         if interval <= 0:
             return
             
