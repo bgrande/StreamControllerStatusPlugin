6997bd994a3792160228eb34946030836f64aa64
Benedikt Grande <dev@bgrande.de>
2026-01-04 12:33:21 +0100

    feat(color): add different settings
    add color for no match;
    also add type (web/local) chooser;
    extract values and keys into constants.

--- actions/StatusAction.py
+++ actions/StatusAction.py
@@ -13,9 +13,22 @@ from src.backend.PluginManager.PluginBase import PluginBase
 
 # Import gtk modules
 import gi
+
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw, Gio, Gdk
+
+NOMATCH_BG_COLOR = "nomatch_bg_color"
+MATCH_BG_COLOR = "match_bg_color"
+INTERVAL = "interval"
+HEADERS = "headers"
+TARGET = "target"
+MATCH_VALUE = "match_value"
+MATCH_MODE = "match_mode"
+MATCH_MODE_STATUS_CODE = "Status Code"
+TYPE = "type"
+TYPE_WEB = "web"
+TYPE_LOCAL = "local"
 
 class StatusAction(ActionBase):
     def __init__(self, *args, **kwargs):
@@ -29,24 +42,22 @@ class StatusAction(ActionBase):
     def on_ready(self):
         # Initialize default settings if not present
         defaults = {
-            "type": "web",  # web, local
-            "target": "https://google.com",
-            "interval": 0,
-            "match_value": "200",
-            "match_mode": "Status Code",
-            "match_bg_color": [0, 255, 0, 255],
+            TYPE: TYPE_WEB,  # web, local
+            TARGET: "https://google.com",
+            INTERVAL: 0,
+            MATCH_VALUE: "200",
+            MATCH_MODE: MATCH_MODE_STATUS_CODE,
+            MATCH_BG_COLOR: [0, 255, 0, 255],
             "match_text_color": [255, 255, 255, 255],
             "match_label": "Online",
             "match_image": "",
-            "nomatch_bg_color": [255, 0, 0, 255],
+            NOMATCH_BG_COLOR: [255, 0, 0, 255],
             "nomatch_text_color": [255, 255, 255, 255],
             "nomatch_label": "Offline",
             "nomatch_image": "",
-            "username": "",
-            "password": "",
             "return_type": "background_color",  # background_color, text, text_color, background_image
             "return_handler": "",
-            "headers": "{}",
+            HEADERS: "{}",
         }
 
         self.settings = self.get_settings()
@@ -84,16 +95,16 @@ class StatusAction(ActionBase):
         self.last_check_time = time.time()
 
         settings = self.get_settings()
-        command_type = settings.get("type", "web")
-        target = settings.get("target", "")
-        headers = settings.get("headers", "{}")
+        command_type = settings.get(TYPE, TYPE_WEB)
+        target = settings.get(TARGET, "")
+        headers = settings.get(HEADERS, "{}")
 
         result = ""
         success = False
         status_code = -1
         
         try:
-            if command_type == "web":
+            if command_type == TYPE_WEB:
                 try:
                     request = urllib.request.Request(target, headers=headers)
                     with urllib.request.urlopen(request, timeout=10) as response:
@@ -118,13 +129,13 @@ class StatusAction(ActionBase):
             self.is_checking = False
 
     def evaluate_result(self, result: str, status_code: int, success: bool):
-        match_mode = self.settings.get("match_mode", "Status Code")
-        match_value = self.settings.get("match_value", "")
+        match_mode = self.settings.get(MATCH_MODE, MATCH_MODE_STATUS_CODE)
+        match_value = self.settings.get(MATCH_VALUE, "")
         
         is_match = False
 
         # todo: the following has to be rethought and redone!
-        if match_mode == "Status Code":
+        if match_mode == MATCH_MODE_STATUS_CODE:
             is_match = str(status_code) == match_value
         elif match_mode == "Contains":
             is_match = match_value in result
@@ -166,11 +177,16 @@ class StatusAction(ActionBase):
 
     def load_config_defaults(self):
         settings = self.get_settings()
-        self.target_entry.set_text(settings.get("target", ""))  # Does not accept None
-        self.headers_entry.set_text(settings.get("headers", "{}"))
-        self.auto_fetch.set_value(settings.get("interval", 0))
 
-        color_list = settings.get("match_bg_color", [0, 255, 0, 255])
+        # Set dropdown index: 0 for web, 1 for local
+        selected_type = settings.get(TYPE, TYPE_WEB)
+        self.type_dropdown.set_selected(0 if selected_type == TYPE_WEB else 1)
+
+        self.target_entry.set_text(settings.get(TARGET, ""))  # Does not accept None
+        self.headers_entry.set_text(settings.get(HEADERS, "{}"))
+        self.auto_fetch.set_value(settings.get(INTERVAL, 0))
+
+        color_list = settings.get(MATCH_BG_COLOR, [0, 255, 0, 255])
         rgba = Gdk.RGBA()
         rgba.red = color_list[0] / 255.0
         rgba.green = color_list[1] / 255.0
@@ -178,51 +194,92 @@ class StatusAction(ActionBase):
         rgba.alpha = color_list[3] / 255.0
         self.match_bg_button.set_rgba(rgba)
 
+        color_list = settings.get(NOMATCH_BG_COLOR, [0, 255, 0, 255])
+        rgba = Gdk.RGBA()
+        rgba.red = color_list[0] / 255.0
+        rgba.green = color_list[1] / 255.0
+        rgba.blue = color_list[2] / 255.0
+        rgba.alpha = color_list[3] / 255.0
+        self.nomatch_bg_button.set_rgba(rgba)
+
     def get_custom_config_area(self):
         return Gtk.Label(label="Add your custom status calls here")
 
     def get_config_rows(self) -> list:
+        # dropdown for type selection (web/local)
+        self.type_list = Gtk.StringList.new([TYPE_WEB, TYPE_LOCAL])
+        self.type_dropdown = Adw.ComboRow(
+            title="Target Type",
+            model=self.type_list
+        )
+
+        # set target url/path
         self.target_entry = Adw.EntryRow(title="URL (i.e. https://google.com) or application path (i.e. /usr/bin/myscript)")
+
+        # set headers for request
         self.headers_entry = Adw.EntryRow(title="Header (json)")
+
+        # set auto fetch interval
         self.auto_fetch = Adw.SpinRow.new_with_range(step=1, min=0, max=3600)
         self.auto_fetch.set_title("Auto Interval (s)")
         self.auto_fetch.set_subtitle("0 to disable")
 
-        # Create the Color Picker Row
-        self.match_bg_row = Adw.ActionRow(title="Match Background Color")
+        # select background color for success
+        self.match_bg_row = Adw.ActionRow(title="Success Background Color")
         color_dialog = Gtk.ColorDialog(with_alpha=True)
         self.match_bg_button = Gtk.ColorDialogButton(dialog=color_dialog)
         self.match_bg_button.set_valign(Gtk.Align.CENTER)
-
-        # 3. Add button as a suffix to the Adw.ActionRow
         self.match_bg_row.add_suffix(self.match_bg_button)
 
+        # select background color for failure
+        self.nomatch_bg_row = Adw.ActionRow(title="Error Background Color")
+        color_dialog = Gtk.ColorDialog(with_alpha=True)
+        self.nomatch_bg_button = Gtk.ColorDialogButton(dialog=color_dialog)
+        self.nomatch_bg_button.set_valign(Gtk.Align.CENTER)
+        self.nomatch_bg_row.add_suffix(self.nomatch_bg_button)
+
+        # now loading default config
         self.load_config_defaults()
 
         # Connect signals
+        self.type_dropdown.connect("notify::selected", self.on_type_changed)
         self.target_entry.connect("notify::text", self.on_target_changed)
         self.headers_entry.connect("notify::text", self.on_headers_changed)
         self.auto_fetch.connect("notify::value", self.on_interval_changed)
         self.match_bg_button.connect("notify::rgba", self.on_match_bg_changed)
+        self.nomatch_bg_button.connect("notify::rgba", self.on_nomatch_bg_changed)
 
-        return [self.target_entry, self.headers_entry, self.auto_fetch, self.match_bg_row]
+        return [
+            self.type_dropdown,
+            self.target_entry,
+            self.headers_entry,
+            self.auto_fetch,
+            self.match_bg_row,
+            self.nomatch_bg_row
+        ]
 
     def on_target_changed(self, entry, *args):
-        self.on_text_changed(entry, "target")
+        self.on_text_changed(entry, TARGET)
 
     def on_headers_changed(self, entry, *args):
-        self.on_text_changed(entry, "headers")
+        self.on_text_changed(entry, HEADERS)
 
     def on_interval_changed(self, entry, *args):
         settings = self.get_settings()
-        settings["interval"] = entry.get_value()
+        settings[INTERVAL] = entry.get_value()
         self.set_settings(settings)
 
     def on_match_bg_changed(self, entry, *args):
+        self.color_changed(entry, MATCH_BG_COLOR)
+
+    def on_nomatch_bg_changed(self, entry, *args):
+        self.color_changed(entry, NOMATCH_BG_COLOR)
+
+    def color_changed(self, entry, key):
         rgba = entry.get_rgba()
         settings = self.get_settings()
         # Store as [R, G, B, A] in 0-255 range to match your defaults
-        settings["match_bg_color"] = [
+        settings[key] = [
             int(rgba.red * 255),
             int(rgba.green * 255),
             int(rgba.blue * 255),
